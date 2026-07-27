306c9bc3f9c0ec928b792f37be86f3ee2145b4d3
K2 <quantumgallery00gmal.com>
2026-07-27 17:44:38 +0100

    move index.ts to project root for cleaner entry point

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import type { ExtensionAPI } from '@earendil-works/pi-coding-agent';
-import { createRobustEditTool } from './tool';
+import { createRobustEditTool } from './src/tool';
 
 /**
  * Pi extension entry point.
